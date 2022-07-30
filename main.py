83463fc83deffbfd5553078a8074145cfb385aef
Mayu Shinde <97580421+Mayur0106@users.noreply.github.com>
2022-07-30 23:55:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def printBorad(xstate,zstate):
     zero = 'x'if xstate[0] else('O' if zstate[0] else 0 )
     one = 'x'if xstate[1] else('O' if zstate[1] else 1 )
     two = 'x'if xstate[2] else('O' if zstate[2] else 2 )
-    three = 'x'if xstate[3] else('O' if zstate[3] else 3)
+    three = 'x'if xstate[3] else('O' if zstate[3] else 3 )
     four = 'x'if xstate[4] else('O' if zstate[4] else 4)    
     five = 'x'if xstate[5] else('O' if zstate[5] else 5)
     six = 'x'if xstate[6] else('O' if zstate[6] else 6)
